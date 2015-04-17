49c1b7114dd0e75cb4dbcfd4de9bd82ead24546e
Donald Ness <donald.ness@gmail.com>
2015-04-17 16:24:36 -0600

    Remove compatibility detection, and refactor listeners to avoid unnecessary calls

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
 var through = require('through2')
 
 function ChromeRuntimeStream(port) {
+  var connected = true
+
   var stream = through.obj(function (msg, enc, next) {
-    port.postMessage(msg)
+    if (connected) {
+      port.postMessage(msg)
+    }
     next()
   },
-  function (done) {
-    port.disconnect()
+  function flush(done) {
+    if (connected) {
+      removeListeners()
+      connected = false
+      port.disconnect()
+    }
     done()
   })
 
-  port.onMessage.addListener(function (msg) {
-    stream.push(msg)
-  })
+  addListeners()
 
-  port.onDisconnect.addListener(function () {
+  function onMessage(msg) {
+    stream.push(msg)
+  }
+
+  function onDisconnect() {
+    removeListeners()
+    connected = false
     stream.end()
-  })
+  }
+
+  function addListeners() {
+    port.onMessage.addListener(onMessage)
+    port.onDisconnect.addListener(onDisconnect)
+  }
+
+  function removeListeners() {
+    port.onMessage.removeListener(onMessage)
+    port.onDisconnect.removeListener(onDisconnect)
+  }
 
   return stream
 }
 
 module.exports = ChromeRuntimeStream
-
-module.exports.detectCompatibility = function (callback) {
-  if (!chrome) {
-    callback && callback(new Error('Chrome browser not detected.'))
-    return false
-  } else if (!chrome.runtime) {
-    callback && callback(new Error('Chrome runtime not detected.'))
-    return false
-  } else if (!typeof chrome.runtime.connect === 'function') {
-    callback && callback(new Error('Chrome version 26 or higher not detected.'))
-    return false
-  } else {
-    callback && callback(null)
-    return true
-  }
-}
